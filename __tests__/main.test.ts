61d355cd5c5eb22b21931b821e689560251da046
Edoardo Tenani <edoardo.tenani@protonmail.com>
2020-07-17 00:23:33 +0200

    add some tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,54 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import simpleGit, {SimpleGit, SimpleGitOptions} from 'simple-git'
+import {dirSync} from 'tmp'
+import {execSync} from 'child_process'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+import * as action from '../src/action'
+
+// setup test environment
+const testDir = dirSync()
+console.log('Dir: ', testDir.name)
+// change WD to test folder
+process.chdir(testDir.name)
+const git: SimpleGit = simpleGit()
+
+test('git credential helper is configured', async () => {
+  await git.init()
+  action.gitConfigureCredentialCacheHelper(900)
+  git.addConfig('credential.helper', 'cache')
+
+  const confs = await git.listConfig()
+  expect(confs.values['.git/config']['credential.helper']).toEqual('cache')
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('set git credentials', async () => {
+  action.gitCredentialCacheStore('https', 'github.com', 'bob', 's3cre7')
+
+  const result = execSync(
+    `echo "protocol=https\nhost=github.com\nusername=bob" | git credential-cache get`
+  )
+  expect(`${result}`).toEqual(`username=bob\npassword=s3cre7\n`)
+  execSync(
+    `echo "protocol=https\nhost=github.com\nusername=bob" | git credential-cache erase`
+  )
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
+// test('test runs', () => {
+//   process.env['INPUT_TIMEOUT'] = '500'
+//   process.env['INPUT_PROTOCOL'] = 'https'
+//   process.env['INPUT_HOST'] = 'github.com'
+// 
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   const options: cp.ExecSyncOptions = {
+//     env: process.env
+//   }
+// 
+//   const somethingSpy = jest.spyOn(action, 'gitConfigureCredentialCacheHelper')
+// 
+//   console.log(cp.execSync(`node ${ip}`, options).toString())
+// 
+//   expect(somethingSpy).toHaveBeenCalledWith(500)
+// })
